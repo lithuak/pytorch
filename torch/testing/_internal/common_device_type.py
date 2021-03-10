e851aed3eea6f12c9db1a0403f010dca0f747230
Edward Z. Yang <ezyang@fb.com>
2021-03-10 12:13:43 -0800

    Add meta device to generic device testing framework, skip NotImplementedError
    With this, under the meta device, 101 tests passed and 16953 skipped.
    It ain't much, but it's a start.
    
    Some various bits and bobs:
    - NotImplementedError suppression at test level is implemented
      in the same way as CUDA memory leak check, i.e., by wrapping
      test methods and monkeypatching them back in.
    - I had to reimplement assertRaises/assertRaisesRegex from scratch to
      ignore NotImplementedError when _ignore_not_implemented_error is True.
      The implementation relies on a small amount of private API that hasn't
      changed since 2010
    - expectedAlertNondeterministic doesn't really work so I skipped them
      all; there's probably a way to do it better
    
    I tested this using `pytest --disable-warnings --tb=native -k meta --sw
    test/*.py` and a pile of extra patches to make collection actually work
    (lol).
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    
    ghstack-source-id: 27d9770f9d5834f19a548fb83b6bd13bb36cc056
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/53682

--- torch/testing/_internal/common_device_type.py
+++ torch/testing/_internal/common_device_type.py
@@ -346,6 +346,15 @@ class CPUTestBase(DeviceTypeTestBase):
     def _should_stop_test_suite(self):
         return False
 
+# The meta device represents tensors that don't have any storage; they have
+# all metadata (size, dtype, strides) but they don't actually do any compute
+class MetaTestBase(DeviceTypeTestBase):
+    device_type = 'meta'
+    _ignore_not_implemented_error = True
+
+    def _should_stop_test_suite(self):
+        return False
+
 class CUDATestBase(DeviceTypeTestBase):
     device_type = 'cuda'
     _do_cuda_memory_leak_check = True
@@ -399,8 +408,10 @@ def get_device_type_test_bases():
                 test_bases.append(CUDATestBase)
         else:
             test_bases.append(CPUTestBase)
+            test_bases.append(MetaTestBase)
     else:
         test_bases.append(CPUTestBase)
+        test_bases.append(MetaTestBase)
         if torch.cuda.is_available():
             test_bases.append(CUDATestBase)
 
@@ -585,6 +596,12 @@ class skipCUDAIf(skipIf):
 
     def __init__(self, dep, reason):
         super().__init__(dep, reason, device_type='cuda')
+
+# Skips a test on Meta if the condition is true.
+class skipMetaIf(skipIf):
+
+    def __init__(self, dep, reason):
+        super().__init__(dep, reason, device_type='meta')
 
 def _has_sufficient_memory(device, size):
     if torch.device(device).type == 'cuda':
@@ -915,3 +932,6 @@ def skipCUDAIfCudnnVersionLessThan(version=0):
 
 def skipCUDAIfNoCudnn(fn):
     return skipCUDAIfCudnnVersionLessThan(0)(fn)
+
+def skipMeta(fn):
+    return skipMetaIf(True, "test doesn't work with meta tensors")(fn)
